e75926f80dd7796cf6c275fa7d467ce51f14cf0d
YannGuerin-app <yannguerin@yahoo.com>
2025-04-27 22:47:35 +0200

    Remove Tailwind CSS and related config

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-// postcss.config.cjs
-module.exports = {
-    plugins: {
-        // on appelle cette entrée : '@tailwindcss/postcss'
-        '@tailwindcss/postcss': {},
-        autoprefixer: {},
-    }
-}
